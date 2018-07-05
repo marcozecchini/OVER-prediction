08a54254f41dbb7a7cef8344d2ee68d48f10207d
marcozecchini <marcozecchini.2594@gmail.com>
2018-07-06 00:36:09 +0200

    multilinear first implementation

--- MultilinearRegression/multilinear_regression.py
+++ MultilinearRegression/multilinear_regression.py
@@ -1,15 +1,15 @@
 import datetime as dt
 import matplotlib.pyplot as plt
-import sklearn.svm as svm
-import calendar
+import sklearn.linear_model as lm
+import statistics as st
 
-def mean_accuracy_with_confidence_interval(predict_array, interval, Y_test):
+def mean_accuracy_with_confidence_interval(predict_array, interval_size, Y_test):
     accuracy = 0
     for hour in range(0,len(predict_array)):
-        if (predict_array[hour] + interval >= Y_test[hour]) and (predict_array[hour] - interval <= Y_test[hour]):
+        value_interval = predict_array[hour] * interval_size
+        if (predict_array[hour] + value_interval >= Y_test[hour]) and (predict_array[hour] - value_interval <= Y_test[hour]):
             accuracy += 1
     return accuracy / 24
-
 
 def prepare_model_and_predict(values, dates, consumptions, predicted):
     for element in values:
@@ -17,19 +17,18 @@ def prepare_model_and_predict(values, dates, consumptions, predicted):
         temp = element.split(",")
         date = dt.datetime.fromtimestamp(int(temp[0]) / 1000)
         consumption = float(temp[1])
+        #consumption of the same hour 24 hour before
+        x_vector += [consumptions[-24] if len(consumptions) > 24 else 0]
 
-        if (calendar.weekday(date.year, date.month, date.day) < 5):
-            x_vector.append(1)
-        else:
-            x_vector.append(0)
-
-        if len(consumptions) > 48:
-            x_vector += [consumptions[-i] for i in range(1, 49)]
-        elif len(consumptions) == 0:
-            x_vector += [5 for _ in range(len(consumptions), 48)]
-        else:
-            x_vector += [consumptions[-i] for i in range(1, len(consumptions))]
-            x_vector += [5 for _ in range(len(consumptions), 49)]
+        #median consumption of the same hour during the past week
+        vect_temp = []
+        for i in range(1,8):
+            if len(consumptions) > 24*i:
+                vect_temp += [consumptions[-24*i]]
+            else:
+                vect_temp += [0]
+        x_vector += [st.median(vect_temp)]
+        #TODO MANCA LA TEMPERATURA
 
         if (date.year == 2018 and date.month == 5 and date.day == 31):
             X_test[date.hour].append(x_vector)
@@ -44,18 +43,21 @@ def prepare_model_and_predict(values, dates, consumptions, predicted):
     # now i create one svm for each hour
     train_and_predict(predicted)
 
-
+#di nuovo calcolo ora per ora, alleno un modello per ogni ora?
 def train_and_predict(predicted):
     for hour in X_train:
-        SVR_model = svm.SVR(kernel='rbf', C=3e21, gamma=3.5e21).fit(X_train[hour], Y_train[hour])
-        predicted += [SVR_model.predict(X_test[hour])]
-        print("{0} value: {1}".format(hour, predicted[hour]))
+        model = lm.LinearRegression()
+        model.fit(X_train[hour], Y_train[hour])
+
+        predicted += [model.predict(X_test[hour])]
+        print(predicted[hour])
 
 
 file = open("../consumptions.txt") # tutti i consumi dal 1/6/2017 al 1/6/2018 alle due
 line = file.readline().strip()
 building = line.split("\t")[0]
 values = line.split("\t")[1].split(" ")
+
 dates = []
 consumptions = []
 X_train = {}
@@ -70,14 +72,12 @@ for i in range(0,24):
     Y_train[i] = []
 
 prepare_model_and_predict(values, dates, consumptions, predict_y_array)
-accuracy = mean_accuracy_with_confidence_interval(predict_y_array, interval=1e5, Y_test=Y_test)
-
+accuracy = mean_accuracy_with_confidence_interval(predict_y_array, interval_size=0.3, Y_test=Y_test)
 print("Mean accuracy: " + str(accuracy))
+
 plt.plot(dates[-26:-2],consumptions[-26:-2], color='red')
 plt.plot(dates[-26:-2], predict_y_array)
 plt.xlabel('dates')
 plt.ylabel('Kwh')
 plt.gcf().autofmt_xdate()
 plt.show()
-
-#TODO aggiungere temperature al vettore di input, riallenarsi sul giorno successivo, prendere i dati da internet.
